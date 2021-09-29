c1088788702cfe16d279e56d75146968c0e6fd84
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-29 15:00:08 -0500

    Inital Commit16

--- vars/sfdxDeployPipeline.groovy
+++ vars/sfdxDeployPipeline.groovy
@@ -63,6 +63,7 @@ def call(Map parameters = [:]) {
 
                                 def toolbelt = tool 'toolbelt'
                                // shWithStatus("sfdx auth:jwt:grant --instanceurl=${sfInstanceURL} --clientid=${sfConsumerKey} --username=${sfUserName} --jwtkeyfile=${server_key_file} --setdefaultdevhubusername --setalias=${authorizeDevHub}")
+                               echo "Script---- ${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
                                rc = command "${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
                                 if (rc != 0) {
                                     error 'Salesforce dev hub org authorization failed.--- ${rc}'
