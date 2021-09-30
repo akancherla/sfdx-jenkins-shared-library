c0329d4b4ab518bc0a4c0f2c2718e2dab0b46681
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-29 21:48:17 -0500

    Inital Commit21

--- vars/sfdxDeployPipeline.groovy
+++ vars/sfdxDeployPipeline.groovy
@@ -71,9 +71,9 @@ def call(Map parameters = [:]) {
                                   //  shWithStatus("sfdx auth:jwt:grant --instanceurl=${sfInstanceURL} --clientid=${sfConsumerKey} --username=${sfUserName} --jwtkeyfile=${server_key_file} --setdefaultdevhubusername --setalias=${authorizeDevHub}")
                                     //  echo "Script---- ${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
                                   rc = command "sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias ${authorizeDevHub}"
-                                //     if (rc != 0) {
-                                //         error 'Salesforce dev hub org authorization failed.--- ${rc}'
-                                //     }
+                                  if (rc != 0) {
+                                         error 'Salesforce dev hub org authorization failed.--- ${rc}'
+                                  }
                                         deploymentOrg.devHubAlias = authorizeDevHub
                                         echo("Successfully authorized DevHub")
                                         
