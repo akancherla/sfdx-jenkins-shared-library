590b12976497b45ca25d2e42c20fdc42dcd4f59a
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-29 22:07:26 -0500

    Inital Commit21

--- vars/shWithStatus.groovy
+++ vars/shWithStatus.groovy
@@ -4,8 +4,13 @@ def call(script) {
     
     //echo "Script ${script}"
 
-    def status = sh returnStatus: true, script: script
-    if (status != 0) {
-        error "Script ${script} failed: status ${status}"
+   // def status = sh returnStatus: true, script: script
+   if (isUnix()) {
+        return sh(returnStatus: true, script: script);
+    } else {
+        return bat(returnStatus: true, script: script);
     }
+  //  if (status != 0) {
+  //      error "Script ${script} failed: status ${status}"
+  //  }
 }
