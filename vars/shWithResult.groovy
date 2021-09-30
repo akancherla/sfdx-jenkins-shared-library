28f235d25f28fa1b2455027d7791be84f13a0d29
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-30 10:40:38 -0500

    Inital Commit32

--- vars/shWithResult.groovy
+++ vars/shWithResult.groovy
@@ -6,22 +6,23 @@ def call(script) {
     
   //  def json = sh returnStdout: true, script: script
   //  def object = readJSON text: json
-//def json = ''
-//def object = ''
+    def json = ''
+    def object = ''
+    def rc = ''
   if (isUnix()) {
-       def rc1 =  sh returnStdout: true, script: script
-        def json1 = rc1.readLines().drop(1).join(" ")
-        def object1 = readJSON text: json1
-        if (object1.status != 0) {
-            error "Script ${script} failed: status ${object1.status} message: ${object1.message} json: ${json1}"
+        rc =  sh returnStdout: true, script: script
+         json = rc1.readLines().drop(1).join(" ")
+         object = readJSON text: json1
+        if (object.status != 0) {
+            error "Script ${script} failed: status ${object.status} message: ${object.message} json: ${json}"
          }
-        return object1.result
+        return object.result
     } else {
-        def rc =  bat(returnStdout: true, script: script).trim()
-        def json = rc.readLines().drop(1).join(" ")
+         rc =  bat(returnStdout: true, script: script).trim()
+         json = rc.readLines().drop(1).join(" ")
       //  json = rs.readLines()
         echo "json---- ${json}"
-        def object = readJSON text: json
+         object = readJSON text: json
         echo "output---- ${object}"
         if (object.status != 0) {
             error "Script ${script} failed: status ${object.status} message: ${object.message} json: ${json}"
