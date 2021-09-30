b77b1dac6ef8a7da781dba39a9bce3c7ed662c8c
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-29 20:47:03 -0500

    Inital Commit23

--- vars/sfdxDeployPipeline.groovy
+++ vars/sfdxDeployPipeline.groovy
@@ -33,54 +33,60 @@ def call(Map parameters = [:]) {
             // stage("Checkout") {
                      //   checkout(scm: scm)
                    // }
-                    stage("Authenticate to org") {
 
-                        if (authenticateToOrg) {
+                   withEnv(["HOME=${env.WORKSPACE}"]) {
+        
+                        withCredentials([file(credentialsId: sfdxUrlCredentialId, variable: 'server_key_file')]) {
+                            stage("Authenticate to org") {
 
-                            withCredentials([file(credentialsId: sfdxUrlCredentialId, variable: 'SFDX_URL')]) {
-                                def authenticationResult = shWithResult('sfdx force:auth:sfdxurl:store --setalias="$JOB_NAME" --setdefaultusername --sfdxurlfile=$SFDX_URL --json')
-                                deploymentOrg.alias = "${env.JOB_NAME}"
-                                deploymentOrg.username = authenticationResult.username
-                                deploymentOrg.orgId = authenticationResult.orgId
-                                deploymentOrg.instanceUrl = authenticationResult.instanceUrl
-                                echo("Successfully authorized ${authenticationResult.username} with org ID ${authenticationResult.orgId}")
-                            }
+                                if (authenticateToOrg) {
 
-                        }
-                        else {
+                                //    withCredentials([file(credentialsId: sfdxUrlCredentialId, variable: 'SFDX_URL')]) {
+                                        def authenticationResult = shWithResult('sfdx force:auth:sfdxurl:store --setalias="$JOB_NAME" --setdefaultusername --sfdxurlfile=$server_key_file --json')
+                                        deploymentOrg.alias = "${env.JOB_NAME}"
+                                        deploymentOrg.username = authenticationResult.username
+                                        deploymentOrg.orgId = authenticationResult.orgId
+                                        deploymentOrg.instanceUrl = authenticationResult.instanceUrl
+                                        echo("Successfully authorized ${authenticationResult.username} with org ID ${authenticationResult.orgId}")
+                                //    }
 
-                            echo("No Authenticate to org")
+                                }
+                                else {
 
-                        }
-                        
+                                    echo("No Authenticate to org")
 
-                    }
-
-                    stage('Authorize DevHub') {
-
-                        if (authorizeDevHub) {
-                            withCredentials([file(credentialsId: sfdxUrlCredentialId, variable: 'server_key_file')]) {
-
-                             def toolbelt = tool 'toolbelt'
-                             shWithStatus("sfdx auth:jwt:grant --instanceurl=${sfInstanceURL} --clientid=${sfConsumerKey} --username=${sfUserName} --jwtkeyfile=${server_key_file} --setdefaultdevhubusername --setalias=${authorizeDevHub}")
-                             //  echo "Script---- ${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
-                           //    rc = command "${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
-                           //     if (rc != 0) {
-                           //         error 'Salesforce dev hub org authorization failed.--- ${rc}'
-                           //     }
-                                deploymentOrg.devHubAlias = authorizeDevHub
-                                echo("Successfully authorized DevHub")
+                                }
                                 
+
                             }
 
-                        }
-                        else {
+                            stage('Authorize DevHub') {
 
-                            echo("No Authorize to org")
+                                if (authorizeDevHub) {
+                                //    withCredentials([file(credentialsId: sfdxUrlCredentialId, variable: 'server_key_file')]) {
 
+                                    // def toolbelt = tool 'toolbelt'
+                                    shWithStatus("sfdx auth:jwt:grant --instanceurl=${sfInstanceURL} --clientid=${sfConsumerKey} --username=${sfUserName} --jwtkeyfile=${server_key_file} --setdefaultdevhubusername --setalias=${authorizeDevHub}")
+                                    //  echo "Script---- ${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
+                                //    rc = command "${toolbelt}/sfdx force:auth:jwt:grant --instanceurl ${sfInstanceURL} --clientid ${sfConsumerKey} --username ${sfUserName} --jwtkeyfile ${server_key_file} --setdefaultdevhubusername --setalias HubOrg"
+                                //     if (rc != 0) {
+                                //         error 'Salesforce dev hub org authorization failed.--- ${rc}'
+                                //     }
+                                        deploymentOrg.devHubAlias = authorizeDevHub
+                                        echo("Successfully authorized DevHub")
+                                        
+                                //    }
+
+                                }
+                                else {
+
+                                    echo("No Authorize to org")
+
+                                }
+                                    
+                            }
                         }
-                               
-                    }
+                   }
                 }
             
             }
