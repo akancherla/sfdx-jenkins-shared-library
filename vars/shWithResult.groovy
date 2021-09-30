2f650cf471b1bfb010537314ed7a7d4e6277aef4
Atchuta Kancharla <akancharlasfdc@gmail.com>
2021-09-30 10:38:59 -0500

    Inital Commit31

--- vars/shWithResult.groovy
+++ vars/shWithResult.groovy
@@ -10,15 +10,15 @@ def call(script) {
 //def object = ''
   if (isUnix()) {
        def rc1 =  sh returnStdout: true, script: script
-        json1 = rc1.readLines().drop(1).join(" ")
+        def json1 = rc1.readLines().drop(1).join(" ")
         def object1 = readJSON text: json1
         if (object1.status != 0) {
             error "Script ${script} failed: status ${object1.status} message: ${object1.message} json: ${json1}"
          }
         return object1.result
     } else {
-        def rc =  bat returnStdout: true, script: script
-        json = rc.readLines().drop(1).join(" ")
+        def rc =  bat(returnStdout: true, script: script).trim()
+        def json = rc.readLines().drop(1).join(" ")
       //  json = rs.readLines()
         echo "json---- ${json}"
         def object = readJSON text: json
